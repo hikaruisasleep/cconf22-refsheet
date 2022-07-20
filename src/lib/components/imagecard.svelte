6cd302941fa6fd5561d4ce19ccff2e2e1488aa53
Ben Wu <benayaj.2004@gmail.com>
2022-07-20 12:41:49 +0700

    h

---
+++ src/lib/components/imagecard.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let src;
+</script>
+
+<div class="card">
+    <!-- svelte-ignore a11y-missing-attribute -->
+    <img src="{src}" style="height: 350px; width: auto;">
+</div>
+
+<style lang="scss">
+    div.card {
+        width: min-content;
+        padding: 10px;
+        margin: 5px;
+        outline: 1px black solid;
+        border-radius: 5px;
+    }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,55 @@
+<header>
+	<div class="bigtitle">
+		<p class="title">CAPTURE CONFERENCE 2022</p>
+		<p class="subtitle">Design Reference Sheet</p>
+	</div>
+    <nav>
+        <a href="/">home</a>
+        <a href="/inspo">inspirations</a>
+        <a href="/assets">assets</a>
+    </nav>
+</header>
+<slot />
+
+<style lang="scss">
+	@use '../lib/styles/style';
+
+	header {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-around;
+        margin-bottom: 20px;
+		.bigtitle {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            justify-content: center;
+            width: max-content;
+			.title {
+				font-family: 'AirntQuantum';
+				font-size: 2em;
+			}
+			.subtitle {
+				font-family: 'Aquire';
+				font-weight: bold;
+				font-size: 1em;
+			}
+		}
+        nav {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-end;
+            justify-content: center;
+            a {
+                font-family: 'Aquire';
+            }
+        }
+	}
+
+    :global(div.container) {
+        p, a {
+            font-family: 'CMG Sans';
+        }
+    }
+</style>
